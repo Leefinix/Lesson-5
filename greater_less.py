3deab2cb60bfeb646fcf8c9c98c53c59a09ac34a
Leefinix <joaquinnleo@gmail.com>
2024-12-31 15:44:03 +0700

    greater_less

---
+++ greater_less.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a number: "))
+if (i < 15):
+    print("The number is less than 15")
+    print("I'm in the if block")
+else:
+    print("The number is greater than 15")
+    print("I'm in the else block")
+print("I'm not in the if block nor the else block")
