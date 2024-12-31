20848222c60f073a862412814a14f961243c5a40
Leefinix <joaquinnleo@gmail.com>
2024-12-31 15:21:36 +0700

    quiz

---
+++ quiz.py
@@ -0,0 +1,11 @@
+answer1 = input("What year did Minecraft come out?")
+if answer1 == "2009":
+    print("Correct!, You're a real Gamer")
+else:
+    print("Wrong!, Minecraft came out in the year '2009', not", answer1)
+
+answer2 = input("What is the device that moves you're camera in modern games?")
+if answer2 == "Mouse":
+    print("Correct!, You're a real Gamer")
+else:
+    print("Wrong!, The device that moves you're camera is a Mouse, not", answer2)
